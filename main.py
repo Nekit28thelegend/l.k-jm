82dc3b2850d062adce4604cb131ea20d75c7f601
Nekit28thelegend <ignatenkovivan68@gmail.com>
2024-08-22 12:48:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+my_dict = {'klii': 685, 'Toyota': 912, 'Messi': 10, 'BMW': 5, 'huuj': 123}
+print(my_dict)
+print(my_dict['Messi'])
+print(my_dict.get('Lexus'))
+my_dict.update({'Mazda': 283, 'Subaru': 623})
+a = my_dict.pop("klii")
+print(a)
+print(my_dict, "\n")
+him_set = {1, 4, 42,3 ,1, 28, 31, True, False, "Jonty"}
+print(him_set)
+him_set.add(38)
+him_set.add('12')
+him_set.remove(31)
+print(him_set)
